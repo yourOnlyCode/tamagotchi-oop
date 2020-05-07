d8a899291ea939546b28c04bb775cfc0ffca5556
Matthew Mullin <mattymullin@Matthews-MacBook-Pro-2.local>
2020-05-07 18:08:44 -0400

    added newtamagotchi's and console.log the cry functionality

--- tamagotchi.js
+++ tamagotchi.js
@@ -2,26 +2,37 @@ console.log('tamagotchi file is loaded');
 
 //your class declaration here
 class Tamagotchi {
-    constructor(foodInTummy, restedness, health) {
+    constructor(foodInTummy, restedness, health, name, creatureType) {
         this.foodInTummy = 10
         this.restedness = 10
         this.health = 10
+        this.name = name
+        this.creatureType = creatureType
     }
 
     cry() {
         this.foodInTummy--
-        console.log('WAHH!!!!' + ' ' + `...${Tamagotchi} cries.`)
+        console.log('WAHH!!!!' + ' ' + `...${this.name} cries.`)
         console.log('Current food: ' + this.foodInTummy)
     }
 }
 
 
 //create new Tamagotchis
-const loli = new Tamagotchi()
-const peopi = new Tamagotchi()
+const tamaObjOne = new Tamagotchi(10, 10, 10, 'loli', 'ferret')
+const tamaObjTwo = new Tamagotchi(10, 10, 10, 'peopi', 'bunny')
+const tamaObjThree = new Tamagotchi(10, 10, 10, 'jewn', 'bug')
+const tamaObjFour = new Tamagotchi(10, 10, 10, 'flap', 'bird')
 
 
 //test out your Tamagotchies below via console.logs
 
-console.log('loli:', loli)
-console.log('peopi:', peopi)
+console.log(`${tamaObjOne.name}`, tamaObjOne)
+console.log(`${tamaObjTwo.name}`, tamaObjTwo)
+console.log(`${tamaObjThree.name}`, tamaObjThree)
+console.log(`${tamaObjFour.name}`, tamaObjFour)
+
+console.log(`${tamaObjOne.name}`, tamaObjOne.cry())
+console.log(`${tamaObjTwo.name}`, tamaObjTwo.cry())
+console.log(`${tamaObjThree.name}`, tamaObjThree.cry())
+console.log(`${tamaObjFour.name}`, tamaObjFour.cry())
